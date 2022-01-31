0e3a3a1b1b1fecd8af2fe17db7b8b87955d99e20
Paul Howarth <paul@city-fan.org>
2022-01-31 19:50:04 +0000

    Add patch from Gentoo for libpng 1.5 compatibility

--- src/xvinfo.c
+++ src/xvinfo.c
@@ -26,7 +26,7 @@
 #define INFOHIGH 270
 
 /* max length of an Info String */
-#define ISTRLEN 80
+#define ISTRLEN 256
 
 /* baseline of top line of text */
 #define TOPBASE (36 + penn_height/2 + 4 + 8 + ASCENT)
